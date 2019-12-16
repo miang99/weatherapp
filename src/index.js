2a7f8c226c815b87859bf5c2e22d704a551387bf
Jen <jenhoang304@gmail.com>
2019-12-15 20:37:15 -0800

    applying bulma

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { getInput, getInfo } from "./fetch";
 
 document.querySelector("#input").addEventListener("submit", e => {
   e.preventDefault();
+  console.log("hello");
   document.querySelector("#form").style.display = "none";
   getInfo(getInput());
 });
